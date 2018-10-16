fb68a32e0c83d09ae3adabb9c0200c2ee1ef8084
Yuriy Zhigulskiy <yzhigulskiy@tango.me>
2018-10-16 18:31:34 +0300

    fix

--- scripts/startup-script.sh
+++ scripts/startup-script.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+
+sudo apt-get update
+sudo apt-get install -y mongodb-org git
+
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
 sudo su appuser
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
@@ -10,15 +20,6 @@ rvm requirements
 rvm install 2.4.1
 rvm use 2.4.1 --default
 gem install bundler -V --no-ri --no-rdoc
-
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
-
-sudo apt-get update
-sudo apt-get install -y mongodb-org git
-
-sudo systemctl start mongod
-sudo systemctl enable mongod
 
 git clone https://github.com/Artemmkin/reddit.git
 cd reddit && bundle install

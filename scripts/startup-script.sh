f686c62f41cd047130ffa2761e025573a4ef0c9f
Yuriy Zhigulskiy <yzhigulskiy@tango.me>
2018-10-16 17:34:22 +0300

    fix

--- scripts/startup-script.sh
+++ scripts/startup-script.sh
@@ -3,7 +3,7 @@ sudo su -
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 
-source ~/.rvm/scripts/rvm
+#source ~/.rvm/scripts/rvm
 rvm requirements
 
 rvm install 2.4.1
@@ -19,7 +19,7 @@ gem install bundler -V --no-ri --no-rdoc
 
 systemctl start mongod
 systemctl enable mongod
-systemctl status mongod
+#systemctl status mongod
 
 git clone https://github.com/Artemmkin/reddit.git
 cd reddit && bundle install

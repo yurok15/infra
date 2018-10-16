00159981f3224794fd62381f8fdfda59baec1344
Yuriy Zhigulskiy <yzhigulskiy@tango.me>
2018-10-16 17:26:46 +0300

    Remove 'gem' package installation

--- scripts/startup-script.sh
+++ scripts/startup-script.sh
@@ -14,7 +14,7 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 
 apt-get update
-apt-get install -y mongodb-org git gem
+apt-get install -y mongodb-org git
 gem install bundler -V --no-ri --no-rdoc
 
 systemctl start mongod
